44d1e357bfcc9e03c7f539b2c704636e2fb55cfd
Cody Frazer <cody.frazer@cph.org>
2011-02-16 15:45:51 -0600

    use robust generation

--- lib/generators/api_controller/templates/api_controller.rb
+++ lib/generators/api_controller/templates/api_controller.rb
@@ -3,13 +3,25 @@ class Api::V<%= version %>::<%= model.capitalize %>ApiController < ApplicationCo
   # GET /api/v<%= version %>/<%= model %>
   # GET /api/v<%= version %>/<%= model %>.xml
   # GET /api/v<%= version %>/<%= model %>.json
-  # def index
-  #   @<%= model %> = <%= model.classify %>.all
+  def index
+    @<%= model %> = <%= model.classify %>.all
 
-  #   respond_to do |format|
-  #     format.xml {render :xml => @<%= model %>}
-  #     format.json {render :json => @<%= model %>}
-  #   end
-  # end
+    respond_to do |format|
+      format.xml {render :xml => @<%= model %>}
+      format.json {render :json => @<%= model %>}
+    end
+  end
+
+  # GET /api/v<%= version %>/<%= model %>/1
+  # GET /api/v<%= version %>/<%= model %>/1.xml
+  # GET /api/v<%= version %>/<%= model %>/1.json
+  def show
+    @<%= model.singularize %> = <%= model.classify %>.find(params[:id])
+
+    respond_to do |format|
+      format.xml {render :xml => @<%= model.singularize %>}
+      format.json {render :json => @<%= model.singularize %>}
+    end
+  end
 
 end
